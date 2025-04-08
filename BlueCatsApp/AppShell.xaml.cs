cca2ce9a4b1886f61081c3c85c79fccd98e1a8bc
ShrNme <ianzhoopes@gmail.com>
2025-04-08 14:03:54 -0400

    Made MAUI template

---
+++ BlueCatsApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlueCatsApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BlueCatsApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BlueCatsApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
